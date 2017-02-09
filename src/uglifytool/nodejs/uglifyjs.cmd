9303650e3612a3425e34d01a1547ca150338296c
advence-liz <1012515183@qq.com>
2017-02-09 14:49:40 +0800

    参悟 %~dp0

--- src/uglifytool/nodejs/uglifyjs.cmd
+++ src/uglifytool/nodejs/uglifyjs.cmd
@@ -1,5 +1,8 @@
 @IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\node_modules\uglify-js\bin\uglifyjs" %*
+ ::  %n %* 为参数
+ ::echo %0 %0代表本批处理文件，其他的限定是基于对本文件的修饰（或者for 中代表文件的变量）
+ ::echo %~f0
+  %~dp0\node.exe  %~dp0\node_modules\uglify-js\bin\uglifyjs %*
 ) ELSE (
-  node  "%~dp0\node_modules\uglify-js\bin\uglifyjs" %*
+  node  %~dp0\node_modules\uglify-js\bin\uglifyjs %*
 )
